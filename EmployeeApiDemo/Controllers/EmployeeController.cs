d49db3857016a4c8c51ae024af7c85ecf9483ed4
Kamal Kumar Rana <kamal.rana@sentientgeeks.com>
2026-05-18 13:08:40 +0530

    Initial commit

---
+++ EmployeeApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApiDemo.Controllers
+{
+    [ApiController]
+
+    [Route("api/[controller]")]
+
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetEmployees()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Kamal",
+                    Salary = 50000
+                },
+
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Rahul",
+                    Salary = 60000
+                }
+            };
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ EmployeeApiDemo/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeApiDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
